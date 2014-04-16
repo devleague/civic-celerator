ecef07b3c267e5859842e24908ccb4958a39b2b9
kingtak <kingtak.w@gmail.com>
2014-04-16 09:31:32 -1000

    finished the pie chart

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,8 +6,8 @@ var myApp = angular.module('myApp.controllers', []);
 
 myApp.controller('customController', ['$scope',
   function($scope) {
-    var industrymoney = [3, 3, 2, 1, 2, 4];
-    var data = {industrymoney: industrymoney}
+    var industrymoneya = [3, 23, 12, 15, 24, 34];
+    var data = {industrymoney: industrymoneya}
     $scope.pData = data;
   }
-]);
+])
--- app/js/directives.js
+++ app/js/directives.js
@@ -13,7 +13,6 @@ myApp.directive('pchart', function($window) {
 
       var width = d3.select(element[0]).node().offsetWidth;
       var height = d3.select(element[0]).node().offsetHeight;
- 
       var svg = d3.select(element[0])
         .append("svg")
         .attr('width', width)
@@ -23,16 +22,14 @@ myApp.directive('pchart', function($window) {
 
       //if the window gets resized
       window.onresize = function() {
-        scope.$apply();
-      }
+        scope.render();
+      };
 
       //watch the window the angular way
       scope.$watch(function() {
-        return angular.element($window)[0].innerWidth;
+        return angular.element(window)[0].innerWidth;
       }, function() {
-      console.log( scope.pData.industrymoney);
-
-        scope.render(scope.pData.industrymoney);
+        scope.render(scope.pData.industymoney);
       });
 
       //continue watching for new values
@@ -41,7 +38,7 @@ myApp.directive('pchart', function($window) {
       }, true);
 
       scope.render = function(data) {
-
+        console.log("us");
         //remove the elements (after rerender)
         svg.selectAll("*").remove();
         //for a bar graph
@@ -49,30 +46,31 @@ myApp.directive('pchart', function($window) {
         var margin = 5;
         //defining the width of the svg reactively
         var radius = Math.min(width, height) / 2;
+        var color = d3.scale.ordinal()
+          .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
         var arc = d3.svg.arc()
-          .outerRadius(radius - margin)
+          .outerRadius(radius - 10)
           .innerRadius(0);
 
         var pie = d3.layout.pie()
           .sort(null)
-          .value(function(data) {
-            return data;
+          .value(function(data, i) {
+            console.log(data);
+            return scope.pData.industrymoney[i];
           })
 
 
-        scope.pData.forEach(function(d) {
-          d += d;
-        });
 
         var g = svg.selectAll(".arc")
-          .data(pie(data))
+          .data(pie(scope.pData.industrymoney))
           .enter()
           .append('g')
           .attr('class', 'arc');
 
         g.append('path')
-          .attr('d', arc);
+          .attr('d', arc)
+          .style("fill", function(d, i) { return color(scope.pData.industrymoney[i]); });
 
         g.append('text')
           .attr('transform', function(d) {
@@ -80,79 +78,56 @@ myApp.directive('pchart', function($window) {
           })
           .attr('dy', '.35em')
           .style('text-anchor', 'middle')
-          .text(function(d) {
-            return d.data.industry;
-          });
+          .text(function(d,i) {
+            return scope.pData.industrymoney[i];
+          })
+          .attr('fill','white');
+      }
+    }
+  }
+});
+
+myApp.directive('pchart', function($window) {
+  return {
+    restrict: 'A',
+    //happens when everything is associated and attached to the dom
+    link: function(scope, element, attrs) {
+
+      var width = d3.select(element[0]).node().offsetWidth;
+      var height = d3.select(element[0]).node().offsetHeight;
+      var svg = d3.select(element[0])
+        .append("svg")
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', "translate(" + width / 2 + ',' + height / 2 + ")");
+
+      //if the window gets resized
+      window.onresize = function() {
+        scope.render();
+      };
+
+      //watch the window the angular way
+      scope.$watch(function() {
+        return angular.element(window)[0].innerWidth;
+      }, function() {
+        scope.render(scope.pData.industymoney);
+      });
+
+      //continue watching for new values
+      scope.$watch('lData', function(newVals, oldVals) {
+        return scope.render(newVals);
+      }, true);
+
+      scope.render = function(data) {
+        console.log("us");
+        //remove the elements (after rerender)
+        svg.selectAll("*").remove();
+        //for a bar graph
+        // var width, height, max;
       }
     }
   }
 });
 
 
-// myApp.directive('d3LineChart', ['d3Service',
-//   function(d3Service) {
-//     return {
-//       restrict: 'A',
-//       //happens when everything is associated and attached to the dom
-//       link: function(scope, element, attrs) {
-
-//         d3Service.d3().then(function(d3) {
-
-//           //if the window gets resized
-//           window.onresize = function() {
-//             scope.$apply();
-//           }
-
-//           //watch the window the angular way
-//           scope.$watch(function() {
-//             return angular.element($window)[0].innerWidth;
-//           }, function() {
-//             scope.render(scope.lineData);
-//           });
-
-//           //continue watching for new values
-//           scope.$watch('data', function(newVals, oldVals) {
-//             return scope.render(newVals);
-//           }, true);
-
-//           scope.render = function(data) {
-
-//             //remove the elements (after rerender)
-//             svg.selectAll("*").remove();
-//             //for a bar graph
-//             // var width, height, max;
-//             var margin = 5;
-//             //defining the width of the svg reactively
-//             var width = d3.select(element[0]).node().offsetWidth;
-//             var height = d3.select(element[0]).node().offsetHeight;
-//             var radius = Math.min(width, height) / 2;
-
-//             var x = d3.time.scale()
-//               .range([0, width]);
-
-//             var y = d3.scale.linear()
-//               .range([height, 0]);
-
-//             var xAxis = d3.svg.axis()
-//               .scale(x)
-//               .orient("bottom");
-
-//             var yAxis = d3.svg.axis()
-//               .scale(y)
-//               .orient("left");
-
-
-//             var line = d3.svg.line()
-//               .x(function(data) {
-//                 return x(data.date);
-//               })
-//               .y(function(data) {
-//                 return y(data.close);
-//               })
-
-//           }
-//         });
-//       };
-//     }
-//   } //function inside the directive
-// ])
