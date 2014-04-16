c46356546276415e039781a6bc4e32bf33763257
daddyderek <daddyderekahn@gmail.com>
2014-04-16 13:40:12 -1000

    ready to merge with jcurry

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,44 +3,96 @@
 /* Controllers */
 
 var App = angular.module( 'myApp.controllers', [ 'ui.bootstrap' ] );
-/****************************
-      * Candidate Name
-****************************/
 
+var Candidates = [
+  {
+    first_name: "Donald",
+    last_name: "Trump",
+    party: "sexist",
+    committees: ["NAMBLA","Alpha Gamma Pi", "Skulls", "Daggars"],
+    bills: ["HK432N","123ABC","FUCK23", "KINGTA", "JASON5", "RAYYAR" ]
+  },
+  {
+    first_name: "Kingtak",
+    last_name: "Wong",
+    party: "All The Time",
+    committees: ["Sexy","alcoholics anonymous", "Fight club", "dealers"],
+    bills: [ "123456","abcdef","kingwa", "wongbu", "rawrsd", "poopie" ]
+  },
+  {
+    first_name: "Tyler",
+    last_name: "Boright",
+    party: "Green",
+    committees: ["Hippies","DARPA", "Skulls", "Alii"],
+    bills: ["(.)(.)","qrthfds","artwqtr", "dgsdga", "gjgfhk", "rwewg" ]
+  },
+  {
+    first_name: "Ray",
+    last_name: "Farias",
+    party: "Rock",
+    committees: ["Rambla","gambla", "hammah", "jang"],
+    bills: ["sdfcdN","r4rwec","we23sd", "df42sd", "asdffb", "asfasdg" ]
+  }
+];
+
+
+/****************************
+      * Main Controller
+****************************/
+App.controller( 'MainCtrl', [ '$scope', function ( $scope ) {
+
+  var CurCandidate = 0;
+
+  $scope.firstName  = Candidates[CurCandidate].first_name;
+  $scope.lastName   = Candidates[CurCandidate].last_name;
+  $scope.party      = Candidates[CurCandidate].party;
+  $scope.coms       = Candidates[CurCandidate].committees;
+  $scope.bills      = Candidates[CurCandidate].bills;
+  
+  // right arrow click  
+  $scope.forward    = function () {
+
+    CurCandidate = ( CurCandidate + 1 ) % Candidates.length;
+
+    $scope.firstName  = Candidates[CurCandidate].first_name;
+    $scope.lastName   = Candidates[CurCandidate].last_name;
+    $scope.party      = Candidates[CurCandidate].party;
+    $scope.coms       = Candidates[CurCandidate].committees;
+    $scope.bills      = Candidates[CurCandidate].bills;
+
+  };
+
+  // left arrow click
+  $scope.back       =  function () {
+
+    var wrap = Candidates.length - 1;
+    var pos  = Math.abs( CurCandidate - Candidates.length );
+
+    CurCandidate = pos;
+
+    $scope.firstName  = Candidates[CurCandidate].first_name;
+    $scope.lastName   = Candidates[CurCandidate].last_name;
+    $scope.party      = Candidates[CurCandidate].party;
+    $scope.coms       = Candidates[CurCandidate].committees;
+    $scope.bills      = Candidates[CurCandidate].bills;
+
+  };
+
+}]);
 /****************************
       * Profile Picture
 ****************************/
 App.controller( 'CarouselCtrl', [ '$scope', function ( $scope ) {
 
   $scope.slides = [];
-  $scope.slides.push({ text : 'Cats!', image : 'http://placekitten.com/300/200' });
-  $scope.slides.push({ text : 'Cats!', image : 'http://placekitten.com/301/200' });
-  $scope.slides.push({ text : 'Cats!', image : 'http://placekitten.com/302/200' });
+  $scope.slides.push({ text : 'Cats!', image : 'http://placekitten.com/3ii/2ii' });
+  $scope.slides.push({ text : 'Cats!', image : 'http://placekitten.com/3i1/2ii' });
+  $scope.slides.push({ text : 'Cats!', image : 'http://placekitten.com/3i2/2ii' });
 
   $scope.setActive = function ( idx ) {
 
     $scope.slides[idx].active = true;
 
   };
-
-}]);
-
-/****************************
-      * Committees 
-****************************/
-App.controller('name', ['$scope', function ( $scope ) {
-  
-}])
-
-/****************************
-      * Bills Sponsored
-****************************/
-App.controller( 'TabsCtrl', [ '$scope', function ( $scope ) {
-
-  $scope.tabs = [
-
-    { title : 'Bill1', content : 'HK432N2' },
-    { title : 'Bill2', content : 'MSDF231', disabled : true }
-  ];
 
 }]);
