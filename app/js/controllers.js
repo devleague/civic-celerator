02650d545348f83904e72a231cb3d24e8cd00b2b
daddyderek <daddyderekahn@gmail.com>
2014-04-16 15:49:30 -1000

    pulling again

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,37 +2,40 @@
 
 /* Controllers */
 
-var App = angular.module( 'myApp.controllers', [ 'ui.bootstrap', 'ngTouch' ] );
+var App = angular.module( 'myApp.controllers', [ 'ui.bootstrap' ] );
 
 var Candidates = [
   {
-    first_name: "Donald",
-    last_name: "Trump",
-    party: "sexist",
-    committees: ["NAMBLA","Alpha Gamma Pi", "Skulls", "Daggars"],
-    bills: ["HK432N","123ABC","FUCK23", "KINGTA", "JASON5", "RAYYAR" ],
-    industry: [ ]
+    first_name  : "Donald",
+    last_name   : "Trump",
+    party       : "sexist",
+    picture     : "http://lorempixel.com/output/cats-h-c-200-300-7.jpg",
+    committees  : [ "NAMBLA", "Alpha Gamma Pi", "Skulls", "Daggars" ],
+    bills       : [ "HK432N", "123ABC", "FUCK23", "KINGTA", "JASON5", "RAYYAR" ]
   },
   {
-    first_name: "Kingtak",
-    last_name: "Wong",
-    party: "All The Time",
-    committees: ["Sexy","alcoholics anonymous", "Fight club", "dealers"],
-    bills: [ "123456","abcdef","kingwa", "wongbu", "rawrsd", "poopie" ]
+    first_name  : "Kingtak",
+    last_name   : "Wong",
+    party       : "All The Time",
+    picture     : "http://placekitten.com/g/200/300",
+    committees  : [ "Sexy", "alcoholics anonymous", "Fight club", "dealers" ],
+    bills       : [ "123456","abcdef","kingwa", "wongbu", "rawrsd", "poopie" ]
   },
   {
-    first_name: "Tyler",
-    last_name: "Boright",
-    party: "Green",
-    committees: ["Hippies","DARPA", "Skulls", "Alii"],
-    bills: ["(.)(.)","qrthfds","artwqtr", "dgsdga", "gjgfhk", "rwewg" ]
+    first_name  : "Tyler",
+    last_name   : "Boright",
+    party       : "Green",
+    picture     : "http://placekitten.com/200/300",
+    committees  : [ "Hippies","DARPA", "Skulls", "Alii" ],
+    bills       : [ "(.)(.)","qrthfds","artwqtr", "dgsdga", "gjgfhk", "rwewg" ]
   },
   {
-    first_name: "Ray",
-    last_name: "Farias",
-    party: "Rock",
-    committees: ["Rambla","gambla", "hammah", "jang"],
-    bills: ["sdfcdN","r4rwec","we23sd", "df42sd", "asdffb", "asfasdg" ]
+    first_name  : "Ray",
+    last_name   : "Farias",
+    party       : "Rock",
+    picture     : "http://lorempixel.com/output/cats-h-c-200-300-1.jpg",
+    committees  : ["Rambla","gambla", "hammah", "jang"],
+    bills       : ["sdfcdN","r4rwec","we23sd", "df42sd", "asdffb", "asfasdg" ]
   }
 ];
 
@@ -49,73 +52,88 @@ var Industry = [
 ];
 
 
-/****************************
-      * Main Controller
-****************************/
-App.controller( 'MainCtrl', [ '$scope', function ( $scope ) {
+/**************************************************
+        * Main Controller / index.html
+**************************************************/
+App.controller( 'MainCtrl', [ '$scope',
 
-  var CurCandidate = 0;
-
-  $scope.firstName  = Candidates[CurCandidate].first_name;
-  $scope.lastName   = Candidates[CurCandidate].last_name;
-  $scope.party      = Candidates[CurCandidate].party;
-  $scope.coms       = Candidates[CurCandidate].committees;
-  $scope.bills      = Candidates[CurCandidate].bills;
-  
-  // right arrow click  
-  $scope.forward    = function () {
-
-    CurCandidate = ( CurCandidate + 1 ) % Candidates.length;
-
-    $scope.firstName  = Candidates[CurCandidate].first_name;
-    $scope.lastName   = Candidates[CurCandidate].last_name;
-    $scope.party      = Candidates[CurCandidate].party;
-    $scope.coms       = Candidates[CurCandidate].committees;
-    $scope.bills      = Candidates[CurCandidate].bills;
-
-  };
-
-  // left arrow click
-  $scope.back       =  function () {
-
-    ( CurCandidate === 0 )? CurCandidate = Candidates.length -1 : CurCandidate--;
-
-    $scope.firstName  = Candidates[CurCandidate].first_name;
-    $scope.lastName   = Candidates[CurCandidate].last_name;
-    $scope.party      = Candidates[CurCandidate].party;
-    $scope.coms       = Candidates[CurCandidate].committees;
-    $scope.bills      = Candidates[CurCandidate].bills;
-  
-  };
-
-}]);
-
-<<<<<<< HEAD
-App.controller('customController', [ '$scope',
   function ( $scope ) {
-    var industrymoneya = [3, 23, 12, 15, 24, 34];
-=======
-App.controller('customController', ['$scope',
-  function($scope) {
 
-    $scope.sex = Candidates[28;
-    $scope.entertainment = 15;
-    $scope.politics = 31;
-    $scope.food = 2;
-    $scope.retail = 11;
+    var CurCandidate = 0;
 
-    var industrymoneya = [$scope.sex, $scope.entertainment, $scope.politics, $scope.food, $scope.retail];
->>>>>>> 5854889aa8041235f22689ca92b299868967dc48
-    var data = {industrymoney: industrymoneya}
-    $scope.pData = data;
+    $scope.firstName  = Candidates[CurCandidate].first_name;
+    $scope.lastName   = Candidates[CurCandidate].last_name;
+    $scope.party      = Candidates[CurCandidate].party;
+    $scope.picture    = Candidates[CurCandidate].picture;
+    $scope.coms       = Candidates[CurCandidate].committees;
+    $scope.bills      = Candidates[CurCandidate].bills;
+    
+    // right arrow click  
+    $scope.forward    = function () {
+
+      CurCandidate = ( CurCandidate + 1 ) % Candidates.length;
+
+      $scope.firstName  = Candidates[CurCandidate].first_name;
+      $scope.lastName   = Candidates[CurCandidate].last_name;
+      $scope.party      = Candidates[CurCandidate].party;
+      $scope.picture    = Candidates[CurCandidate].picture;
+      $scope.coms       = Candidates[CurCandidate].committees;
+      $scope.bills      = Candidates[CurCandidate].bills;
+
+    };
+
+    // left arrow click
+    $scope.back         =  function () {
+
+      ( CurCandidate === 0 )? CurCandidate = Candidates.length -1 : CurCandidate--;
+
+      $scope.firstName  = Candidates[CurCandidate].first_name;
+      $scope.lastName   = Candidates[CurCandidate].last_name;
+      $scope.party      = Candidates[CurCandidate].party;
+      $scope.picture    = Candidates[CurCandidate].picture;
+      $scope.coms       = Candidates[CurCandidate].committees;
+      $scope.bills      = Candidates[CurCandidate].bills;
+    
+    };
+
   }
+
 ]);
 
-App.controller('customController2', ['$scope',
+/**************************************************
+                * Bill.html
+**************************************************/
+
+
+App.controller( 'MoneyCtrl', [ '$scope',
+
   function ( $scope ) {
-    var industrymoneya = [15, 4, 42, 8, 23, 16];
-    var data = {industrymoney: industrymoneya}
+
+    //$scope.sex            = Candidates = 28;
+    $scope.entertainment  = 15;
+    $scope.politics       = 31;
+    $scope.food           = 2;
+    $scope.retail         = 11;
+
+    var industrymoneya    = [ $scope.sex, $scope.entertainment, $scope.politics, $scope.food, $scope.retail ];
+    var data              = { industrymoney : industrymoneya };
+
     $scope.pData = data;
+
   }
+
+]);
+
+App.controller( 'IndustryCtrl', [ '$scope', 
+
+  function ( $scope ) {
+
+    var industrymoneya  = [ 15, 4, 42, 8, 23, 16 ];
+    var data            = { industrymoney : industrymoneya };
+
+    $scope.pData        = data;
+
+  }
+
 ]);
 
