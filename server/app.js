d010bb3a82931b2a86c581ef9e7d7a62469a73ef
daddyderek <daddyderekahn@gmail.com>
2014-04-16 19:55:32 -1000

    models and schemas defined

--- server/app.js
+++ server/app.js
@@ -1,72 +1,83 @@
-var restify = require('restify');
-var server = restify.createServer();
+/**************************************
+            * Dependencies
+**************************************/
+
+var restify   = require('restify');
+var server    = restify.createServer();
+var mongoose  = require('mongoose/');
+
 server.use(restify.bodyParser());
-var mongoose = require('mongoose/');
+
+
+/**************************************
+            * Mongoose.js
+***************************************/
+
 // var config = require('./config');
 db = mongoose.connect('mongodb://localhost/text');
-Schema = mongoose.Schema;
 
-var candidate = {
-  first_name        : "firstName",
-  last_name         : "lastName",
-  photo_url         : "http://capitol.bullshit.com",
-  party             : "Democratic/Republican/etc",
-  service_begin     : 2006,
-  service_end       : 2014,
-  committiees       : [ "Finance", "Labor & Public Employment", "Legislative Management" ],
-  sponsored_bills   : [ "MK474", "H3456K", "43474", "21456K" ]
-};
 
-var contributions = {
-  contributor_name  : "name/company name",
-  candidate_name    : "Neil Abercrombie",
-  amount            : 5000000,
-  date_contributed  : "04/12/13",
-  employer          : "Sun Pacific Group LLC",
-  inudstry          : "labor"
-};
-//data schema for each candidate
-var CandidateSchema = new Schema({
-  name: String
+// Mongoose Schemas //
+var candidateSchema = new mongoose.Schema({
+
+  first_name      : String,
+  last_name       : String,
+  photo_url       : String,
+  party           : String,
+  service_begin   : Number,
+  service_end     : Number,
+  committiees     : [String],
+  sponsored_bills : [String]
+
 });
 
+var contributionsSchema = new mongoose.Schema({
 
-//create candidate model in mongodb
-mongoose.model('Candidate', CandidateSchema);
-var Candidate = mongoose.model('Candidate');
+  contributor_name  : String,
+  candidate_name    : String,
+  amount            : Number,
+  employer          : String,
+  industry          : String,
+  date_contributed  : Date,
 
+});
 
-//get all candidates
-// function getCandidates(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   //.find() without arguments to return all candidates
-//   //-1 to denote descending order
-//   Candidate.find().exec(function (arr,data) {
-//     res.send(data);
-//   });
-// }
-
-function getCandidates(req, res, next) {
-  return res.json(candidate);
-}
-
-function getContributions(req, res, next) {
-  return res.json(contributions);
-} 
-// function findOneCandidate(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   Candidate.findOne(_id: _id)
-
-// }
+// Mongoose Models //
+var Candidate     = mongoose.model( 'candidate', candidateSchema );
+var Contributions = mongoose.model( 'contribution', contributionsSchema );
 
 
-//rest verb handling
-server.get('/candidates', getCandidates);
-server.get('/contributions', getContributions);
+// server.get('/api/candidates') //
+function getCandidates ( req, res ) {
+  
+  /* figure out how to get the page number to increment up in the db */
+  var page = req.params.page  
+
+  Candidate.find().sort().skip( 150 * page ).limit( 150 ).exec(
+    function () {
 
 
+    });
+
+
+}// getCanidates
+
+// server.get('/api/contributions') //
+function getContributions ( req, res ) {
+
+
+}// getContributions 
+
+/**************************************
+            * Route Handling
+***************************************/
+
+server.get('/api/candidates', getCandidates);
+server.get('/api/contributions', getContributions);
+
+/**************************************
+            * Server Setup
+***************************************/
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
