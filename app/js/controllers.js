8e0cff3b39212375fe6e2af38bf75c733866e8ef
jahsan87 <jahsan87@gmail.com>
2014-04-16 14:06:43 -1000

    before merge

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -34,7 +34,7 @@ var myApp = angular.module('myApp.controllers', []);
 
 myApp.controller('customController', ['$scope',
   function($scope) {
-    var industrymoneya = [3, 23, 12, 15, 24, 34];
+    var industrymoneya = [3, 23, 12, 15, 24, 34, 16];
     var data = {industrymoney: industrymoneya}
     $scope.pData = data;
   }
